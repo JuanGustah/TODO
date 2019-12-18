510e2e426a281357629208f56fef1e9291fff943
Juan Gustavo <juangt360graus@gmail.com>
2019-12-17 23:21:39 -0300

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Model,TodoItem} from './model'
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,26 +9,52 @@ import {Model,TodoItem} from './model'
 })
 export class AppComponent {
   model=new Model();
+  dataAtual=new Date()
+  Item=new TodoItem("","","","")
+  
 
   getName= () => this.model.user;
   getItensOrdenados(){
     return this.model.items.sort((a,b)=>{
-      return <any>new Date(b.data)-<any>new Date(a.data);
+      return <any>new Date(a.data)-<any>new Date(b.data);
     });
   }
+  getItensUrgentes(){
+    return this.getItensOrdenados().filter(item=>new Date(item.data)<this.dataAtual)
+  }
   getTodoItens=()=> this.getItensOrdenados().filter(item=>!item.done);
-  getPriAlta=()=>this.getTodoItens().filter(atem=>atem.prioridade==="alta")
-  getPriMedia=()=>this.getTodoItens().filter(mtem=>mtem.prioridade==="media")
-  getPriBaixa=()=>this.model.items.filter(btem=>btem.prioridade==="baixa")
+  getPriAlta=()=>this.getTodoItens().filter(atem=>atem.prioridade==="alta" && new Date(atem.data)>this.dataAtual)
+  getPriMedia=()=>this.getTodoItens().filter(mtem=>mtem.prioridade==="media" && new Date(mtem.data)>this.dataAtual)
+  getPriBaixa=()=>this.getTodoItens().filter(btem=>btem.prioridade==="baixa" && new Date(btem.data)>this.dataAtual)
   getItensFeitos=()=> this.model.items.filter(item=>item.done);
 
-  addItem(newItem,prioridade,data){
-    if(newItem != ""){
-      this.model.items.push(new TodoItem(newItem,false,prioridade,data));
+  addItem(){
+    if(this.Item.action!=""&&this.Item.data!=""&&this.Item.prioridade!=""){
+      if(this.model.items==""){
+        this.model.items.push(this.Item);
+        this.Item=new TodoItem("","","","")
+      }
+      else{
+        let variable= this.getTodoItens().filter(a=>a.action==this.Item.action)
+        if(variable==""){
+          this.model.items.push(this.Item);
+          this.Item=new TodoItem("","","","")
+        }
+      }
+      
     }
   }
   corrigirTarefa(itemErrado){
     itemErrado.done="";
+  }
+  removerTarefa(itemParaRemover){
+    this.model.items.splice(this.model.items.indexOf(itemParaRemover),1)
+  }
+  editarTarefa(itemParaEditar){
+    this.model.items.splice(this.model.items.indexOf(itemParaEditar),1)
+    this.Item.action=itemParaEditar.action
+    this.Item.prioridade=itemParaEditar.prioridade
+    this.Item.data=itemParaEditar.data
   }
 
 }
--- src/app/model.ts
+++ src/app/model.ts
@@ -3,12 +3,8 @@ export class Model{
     items;
 
     constructor(){
-        this.user= "Juan"
-        this.items=[new TodoItem("Preparar aula",false,"alta","2019-08-15"),
-                    new TodoItem("Falar com o Trump",false,"media","2019-09-15"),
-                    new TodoItem("Caminhar 10km",false,"baixa","2019-10-15"),
-                    new TodoItem("Dominar o mundo",false,"baixa","2018-11-15")
-    ]
+        this.user= "Erik"
+        this.items=[]
     }
 }
 export class TodoItem{
